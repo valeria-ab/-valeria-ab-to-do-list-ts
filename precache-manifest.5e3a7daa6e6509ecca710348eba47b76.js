003fc6e800468f5bde8ce794d4fefbf7ac9b63b7
valeria-ab <offtotheraces@inbox.ru>
2021-01-13 19:28:15 +0300

    Updates

---
+++ precache-manifest.5e3a7daa6e6509ecca710348eba47b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813c3f656fc619740b635b0663160716",
+    "url": "/to-do-list-ts/index.html"
+  },
+  {
+    "revision": "e3a39252d79292a1575d",
+    "url": "/to-do-list-ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "fd95d23f447b8d601baf",
+    "url": "/to-do-list-ts/static/js/2.06147ebd.chunk.js"
+  },
+  {
+    "revision": "92bfebbc9186106791d506e48cb876d1",
+    "url": "/to-do-list-ts/static/js/2.06147ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a39252d79292a1575d",
+    "url": "/to-do-list-ts/static/js/main.10d266d4.chunk.js"
+  },
+  {
+    "revision": "b5da341ba48785d21603",
+    "url": "/to-do-list-ts/static/js/runtime-main.0be532a2.js"
+  }
+]);
